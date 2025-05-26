9f7ec4b7853e2bc5e15242e2173c0589a498e12b
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2025-05-26 23:43:40 +0300

    add pino logger

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,13 +1,36 @@
-import { Hono } from "hono";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import { auth } from "@/lib/auth";
 import { cors } from "hono/cors";
+import { requestId } from "hono/request-id";
+import { type PinoLogger, pinoLogger } from "hono-pino";
+import { notFound, onError } from "stoker/middlewares";
+import pino from "pino";
+import pretty from "pino-pretty";
 
-const app = new Hono<{
+const app = new OpenAPIHono<{
   Variables: {
     user: typeof auth.$Infer.Session.user | null;
     session: typeof auth.$Infer.Session.session | null;
+    logger: PinoLogger;
   };
 }>();
+
+app
+  .use(
+    requestId({
+      generator: () => Bun.randomUUIDv7(),
+    }),
+  )
+  .use(
+    pinoLogger({
+      pino: pino(
+        {
+          level: process.env.LOG_LEVEL || "info",
+        },
+        process.env.NODE_ENV === "production" ? undefined : pretty(),
+      ),
+    }),
+  );
 
 app.use("*", async (c, next) => {
   const session = await auth.api.getSession({ headers: c.req.raw.headers });
@@ -40,5 +63,14 @@ app.use(
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.get("/error", (c) => {
+  c.status(451);
+  c.var.logger.info("test");
+  throw new Error("test");
+});
+
+app.notFound(notFound);
+app.onError(onError);
 
 export default app;
