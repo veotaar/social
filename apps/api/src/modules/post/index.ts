4dfa6303f77e5f54770fd99d2d8fb30da61a6382
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2025-07-26 03:30:21 +0300

    feat(api): add service for fetching latest posts

--- apps/api/src/modules/post/index.ts
+++ apps/api/src/modules/post/index.ts
@@ -1,24 +1,41 @@
 import { Elysia, t } from "elysia";
 import { db as model } from "@/db/model";
 import { betterAuth } from "../auth";
-import { createPost } from "./service";
+import { createPost, getFeedPosts } from "./service";
 
 const { post } = model.insert;
 
-export const postRoute = new Elysia().use(betterAuth).post(
-  "/post",
-  async ({ body, user }) => {
-    const created = await createPost({
-      userId: user.id,
-      content: body.content,
-    });
+export const postRoute = new Elysia()
+  .use(betterAuth)
+  .post(
+    "/post",
+    async ({ body, user }) => {
+      const created = await createPost({
+        userId: user.id,
+        content: body.content,
+      });
 
-    return created;
-  },
-  {
-    auth: true,
-    body: t.Object({
-      content: post.content,
-    }),
-  },
-);
+      return created;
+    },
+    {
+      auth: true,
+      body: t.Object({
+        content: post.content,
+      }),
+    },
+  )
+  .get(
+    "/post",
+    async ({ user, query }) => {
+      const { cursor } = query;
+      const posts = await getFeedPosts({ currentUserId: user.id, cursor });
+
+      return posts;
+    },
+    {
+      auth: true,
+      query: t.Object({
+        cursor: t.Optional(t.String()),
+      }),
+    },
+  );
--- apps/api/src/modules/post/service.ts
+++ apps/api/src/modules/post/service.ts
@@ -1,5 +1,7 @@
 import { table } from "@/db/model";
 import db from "@/db/db";
+import { and, desc, eq, notInArray, lt } from "drizzle-orm";
+import { block, post, user } from "@/db/schema";
 
 export const createPost = async ({
   userId,
@@ -14,4 +16,76 @@ export const createPost = async ({
     .returning();
 
   return created;
+};
+
+export const getFeedPosts = async ({
+  currentUserId,
+  limit = 10,
+  cursor,
+}: {
+  currentUserId: string;
+  limit?: number;
+  cursor?: string;
+}) => {
+  const blockedUsersSubQuery = db
+    .select({ id: block.blockedId })
+    .from(block)
+    .where(eq(block.blockerId, currentUserId));
+
+  const blockingUsersSubQuery = db
+    .select({ id: block.blockerId })
+    .from(block)
+    .where(eq(block.blockedId, currentUserId));
+
+  const feed = await db
+    .select({
+      post: {
+        id: post.id,
+        content: post.content,
+        createdAt: post.createdAt,
+        likesCount: post.likesCount,
+        commentsCount: post.commentsCount,
+        sharesCount: post.sharesCount,
+      },
+      author: {
+        id: user.id,
+        username: user.username,
+        displayUsername: user.displayUsername,
+        image: user.image,
+      },
+    })
+    .from(post)
+    .where(
+      and(
+        notInArray(post.authorId, blockedUsersSubQuery),
+        notInArray(post.authorId, blockingUsersSubQuery),
+        cursor ? lt(post.id, cursor) : undefined,
+      ),
+    )
+    .orderBy(desc(post.id))
+    .limit(limit + 1)
+    .leftJoin(user, eq(post.authorId, user.id));
+
+  let hasMore = false;
+  let nextCursor: string | null = null;
+
+  if (feed.length > limit) {
+    hasMore = true;
+    feed.pop();
+  }
+
+  if (feed.length > 0) {
+    const lastPostInFeed = feed[feed.length - 1]?.post;
+    if (lastPostInFeed) {
+      nextCursor = lastPostInFeed.id;
+    }
+  }
+
+  return {
+    posts: feed,
+    pagination: {
+      hasMore,
+      nextCursor,
+    },
+  };
 };
