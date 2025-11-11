f211b1d5e42220a1fdde83efe6aa43c307d249e5
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2025-11-12 02:00:24 +0300

    feat(web): add query client to router context
    also enabled view transitions by default

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,15 +1,26 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import {
+  createRootRouteWithContext,
+  Link,
+  Outlet,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Navbar } from "@web/components/Navbar";
+import type { QueryClient } from "@tanstack/react-query";
 
-export const Route = createRootRoute({
-  component: () => (
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
     <>
       <Navbar />
       <Outlet />
       <TanStackRouterDevtools />
       <ReactQueryDevtools />
     </>
-  ),
-});
+  );
+}
