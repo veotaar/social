6f696e5bdf086ec0b974ce9de755abaacbbefa3c
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2025-11-15 16:57:54 +0300

    feat(web): add hook for sending follow requests

---
+++ apps/web/src/hooks/useSendFollowRequest.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { client } from "@web/lib/api-client";
+
+const sendFollowRequest = async (userId: string) => {
+  const { data, error } = await client
+    .users({ userid: userId })
+    .followRequests.post();
+
+  if (error) throw new Error("Failed to send follow request");
+
+  return data;
+};
+
+export const useSendFollowRequest = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (userId: string) => {
+      return sendFollowRequest(userId);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["followRequests"],
+      });
+    },
+  });
+};
