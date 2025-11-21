35b8da549c0243ef3ed55f23323ab9f240d82729
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2025-11-22 00:39:19 +0300

    feat(web): add options button for blocking/unblocking users

---
+++ apps/web/src/components/options-button/OptionsButton.tsx
@@ -0,0 +1,68 @@
+import type { Treaty } from "@elysiajs/eden";
+import { client } from "@web/lib/api-client";
+import { Ellipsis } from "lucide-react";
+import { useBlockUser } from "../blocked-user/useBlockUser";
+import { useUnblockUser } from "../blocked-user/useUnblockUser";
+import { useQueryClient } from "@tanstack/react-query";
+import { cn } from "@web/lib/utils";
+
+const userType = client.users({ userid: "string" }).get;
+
+type User = Treaty.Data<typeof userType>;
+
+export interface OptionsButtonProps {
+  user: User;
+}
+
+export default function OptionsButton({ user }: OptionsButtonProps) {
+  const queryClient = useQueryClient();
+
+  const { mutate: block } = useBlockUser(user.id);
+  const { mutate: unBlock } = useUnblockUser(user.id);
+
+  const handleBlockToggle = () => {
+    if (user.isBlocked) {
+      unBlock(undefined, {
+        onSuccess: () => {
+          queryClient.invalidateQueries({ queryKey: ["user", user.id] });
+          queryClient.invalidateQueries({ queryKey: ["blockedAccounts"] });
+          queryClient.invalidateQueries({ queryKey: ["userPosts", user.id] });
+        },
+      });
+    } else {
+      block(undefined, {
+        onSuccess: () => {
+          queryClient.invalidateQueries({ queryKey: ["user", user.id] });
+          queryClient.invalidateQueries({ queryKey: ["blockedAccounts"] });
+          queryClient.invalidateQueries({ queryKey: ["userPosts", user.id] });
+        },
+      });
+    }
+  };
+
+  return (
+    <div className="dropdown dropdown-end">
+      <button
+        tabIndex={0}
+        type="button"
+        className="btn btn-circle btn-outline btn-sm"
+      >
+        <Ellipsis className="h-5 w-5" />
+      </button>
+      <ul
+        // tabIndex={0}
+        className="dropdown-content menu z-1 min-w-32 rounded-box bg-base-100 p-2 shadow"
+      >
+        <li>
+          <button
+            type="button"
+            onClick={handleBlockToggle}
+            className={cn(user.isBlocked ? "" : "text-error")}
+          >
+            {user.isBlocked ? "Unblock User" : "Block User"}
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
